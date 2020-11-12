359a152f3b5b178e10f863fc1b858faf15e5a97d
sierpien <stepnikagata@gmail.com>
2020-11-12 18:10:35 +0100

    Project complited

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ class Slider {
     this.prevBtn = null;
     this.nextBtn = null;
     this.image = null;
+    this.currentSlide = 0;
+    this.slideArrayLength = 0;
+    this.slideCaption = null;
 
     this.UiSelectors = {
         slide: '[data-slide]',
@@ -20,9 +23,57 @@ class Slider {
         this.nextBtn = document.querySelector(this.UiSelectors.buttonNext);
 
         this.image = document.createElement('img');
-        this.image.setAttribute('src', this.images[0]);
-
+        this.image.classList.add('img-fluid', 'slider_img');
+        // this.image.setAttribute('src', this.images[this.currentSlide]);
+        this.setSlideAttributes(0);
+        this.slideArrayLength = this.images && this.images.length;
+        
+        
+        this.slideCaption = document.createElement('figcaption');
+        this.addCaption();
+        this.slideCaption.classList.add('slider__caption');
+        
         this.slide.appendChild(this.image);
+        this.slide.appendChild(this.slideCaption);
+        
+        this.addListeners();
+        this.disableButtons();
 
+    }
+
+    addListeners(){
+        this.prevBtn.addEventListener('click', () => this.changeSlide(this.currentSlide - 1));
+        this.nextBtn.addEventListener('click', () => this.changeSlide(this.currentSlide + 1));
+
+        document.addEventListener('keydown', (e) =>{
+            if(e.keyCode === 37){
+                this.changeSlide(this.currentSlide - 1);
+            }
+            else if(e.keyCode === 39){
+                this.changeSlide(this.currentSlide + 1);
+            }
+        })
+    }
+
+    disableButtons(){
+        this.currentSlide === 0 ? this.prevBtn.setAttribute('disabled', true) : this.prevBtn.removeAttribute('disabled');
+        this.currentSlide === this.slideArrayLength - 1 ? this.nextBtn.setAttribute('disabled', true) : this.nextBtn.removeAttribute('disabled');
+    }
+
+    changeSlide(index){
+        if(index === -1 || index === this.slideArrayLength) return;
+        this.currentSlide = index;
+        this.setSlideAttributes(index);
+        this.disableButtons();
+        this.addCaption();
+    }
+
+    addCaption(){
+        this.slideCaption.innerText = `${this.currentSlide + 1}/${this.slideArrayLength}`;
+    }
+
+    setSlideAttributes(index){
+        this.image.setAttribute('src', Array.isArray(this.images) && this.images.length && this.images[index]);
+        this.image.setAttribute('alt', `Slide ${index + 1}`);
     }
 }
